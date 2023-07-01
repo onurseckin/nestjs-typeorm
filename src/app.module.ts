fa5a592f73b53fe22d99cfd8abaf0ac4a7d6a675
Onur Seckin Senoglu <onurseckinsenoglu@gmail.com>
2023-07-01 13:41:16 -0700

    postgres init docker-compose typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres', // type of our database
+      host: 'localhost', // CHANGE TO YOUR DB HOST
+      port: 5432, // CHANGE TO YOUR PORT
+      username: 'postgres', // CHANGE TO YOUR USERNAME
+      password: 'pass123', // CHANGE TO YOUR PASSWORD
+      database: 'postgres', // CHANGE TO YOUR DB NAME
+      autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+      synchronize: true, // NEVER USE IN PRODUCTION
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
